7f2e5e0b1070479fce6859f6023e92f4bbbbc99b
srivarmadap <86130560+srivarmadap@users.noreply.github.com>
2022-07-16 13:21:47 -0400

    pygamev1

---
+++ SrivarGame.py
@@ -0,0 +1,33 @@
+import pygame
+import random 
+import sys
+import pygame.locals
+
+#Intialize pygame
+pygame.init()
+
+#Window size
+window_width = 800
+window_height = 600
+
+#Creates game screen
+screen = pygame.display.set_mode((window_width,window_height))
+
+#Title & Icon
+pygame.display.set_caption("Word Game")
+icon = pygame.image.load('puzzle.png')
+pygame.display.set_icon(icon)
+
+#Background
+background = pygame.image.load('background.png') 
+
+
+#Game Loop
+running = True
+while running:
+
+    screen.blit(background, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
