a4dafb930db5f21f3550dfeb10d65747c89f7bea
srivarmadap <86130560+srivarmadap@users.noreply.github.com>
2022-07-16 22:15:22 -0400

    hio

--- timer.py
+++ timer.py
@@ -22,45 +22,42 @@ pygame.display.set_icon(icon)
 background = pygame.image.load('backgroundVS.png') 
 clock = pygame.time.Clock()
 
+#Loading Alphabets
+a_image = 
+b_image = 
+c_image = 
+d_image = 
+e_image = 
+f_image = 
+g_image = 
+h_image = 
+i_image = 
+j_image = 
+k_image = 
+l_image = 
+m_image = 
+n_image = 
+o_image = 
+p_image = 
+q_image = 
+r_image = 
+s_image = 
+t_image = 
+u_image = 
+v_image = 
+w_image = 
+x_image = 
+y_image = 
+z_image = 
 
 
+#Timer Counter
 counter, text = 60, 'Time: 60'.rjust(3)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 font = pygame.font.SysFont('Consolas', 30)
 
-#Buttons
-rows = 1
-cols = 7
-gap = 20
-size = 50
-boxes = []
-
-#color
-white = (255,255,255)
-black = (0,0,0)
-
-for row in range(rows):
-    for col in range(cols):
-        x = ((col * gap) + gap) + (size * col) + 140
-        y = ((row * gap) + gap) + (size * row) + 300
-        box = pygame.Rect(x,y,size,size)
-        boxes.append(box)
-
-
-
-buttons = []
-A = 65
-
-
-for ind, box in enumerate(boxes):
-    letter = chr(A+ind)
-    button = [box, letter]
-    buttons.append(button)
-
-btn_font = pygame.font.SysFont('arial', 3)
 #Game Loop
 running = True
-
 while True:
 
     while running:
@@ -72,15 +69,63 @@ while True:
                 text = "Time:" +str(counter).rjust(3) if counter > 0 else 'Game Over!'
             if event.type == pygame.QUIT:
                 running = False
-        
-        screen.blit(font.render(text, True, (255, 255, 255)), (650, 20))
-        pygame.display.flip()
-        for box, letter in buttons:
-            btn_text = btn_font.render(letter, True, black)
-            btn_rect = btn_text.get_rect(center = (box.x + 20, box.y +20))
-            screen.blit(btn_text, btn_rect)
-            pygame.draw.rect(screen, black, box, 2)
+            #If keystroke is pressed, check what it is
+            if event.type  == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    print('A')
+                if event.key == pygame.K_b:
+                    print('b')
+                if event.key == pygame.K_c:
+                    print('c')
+                if event.key == pygame.K_d:
+                    print('A')
+                if event.key == pygame.K_e:
+                    print('b')
+                if event.key == pygame.K_f:
+                    print('c')
+                if event.key == pygame.K_g:
+                    print('A')
+                if event.key == pygame.K_h:
+                    print('b')
+                if event.key == pygame.K_i:
+                    print('c')
+                if event.key == pygame.K_j:
+                    print('A')
+                if event.key == pygame.K_k:
+                    print('b')
+                if event.key == pygame.K_l:
+                    print('c')
+                if event.key == pygame.K_m:
+                    print('A')
+                if event.key == pygame.K_n:
+                    print('b')
+                if event.key == pygame.K_o:
+                    print('c')
+                if event.key == pygame.K_p:
+                    print('A')
+                if event.key == pygame.K_q:
+                    print('b')
+                if event.key == pygame.K_r:
+                    print('c')
+                if event.key == pygame.K_s:
+                    print('A')
+                if event.key == pygame.K_t:
+                    print('b')
+                if event.key == pygame.K_u:
+                    print('c')
+                if event.key == pygame.K_v:
+                    print('A')
+                if event.key == pygame.K_w:
+                    print('b')
+                if event.key == pygame.K_x:
+                    print('c')
+                if event.key == pygame.K_y:
+                    print('A')
+                if event.key == pygame.K_z:
+                    print('b')
 
+        screen.blit(font.render(text, True, (255, 255, 255)), (620, 20))
+        pygame.display.flip()
         clock.tick(60)
         pygame.display.update()
 
